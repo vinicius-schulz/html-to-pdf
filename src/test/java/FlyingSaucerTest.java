7184ca07f08c4e29dd771c533b4706582ce925d2
Vinicius Miranda Lopes Schulz <vinicius-schulz@hotmail.com>
2020-04-26 17:41:06 -0300

    segundo

---
+++ src/test/java/FlyingSaucerTest.java
@@ -0,0 +1,151 @@
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.w3c.tidy.Tidy;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.*;
+import java.nio.file.FileSystems;
+
+import static com.itextpdf.text.pdf.BaseFont.EMBEDDED;
+import static com.itextpdf.text.pdf.BaseFont.IDENTITY_H;
+import static org.thymeleaf.templatemode.TemplateMode.HTML;
+
+/**
+ * This is a JUnit test which will generate a PDF using Flying Saucer and
+ * Thymeleaf templates. The PDF will display a letter styled with CSS. The
+ * letter has two pages and will contain text and images.
+ * <p>
+ * Simply run this test to generate the PDF. The file is called:
+ * <p>
+ * /test.pdf
+ */
+public class FlyingSaucerTest {
+
+    private static final String OUTPUT_FILE = "test.pdf";
+    private static final String UTF_8 = "UTF-8";
+
+    public void generatePdf() throws Exception {
+
+        // We set-up a Thymeleaf rendering engine. All Thymeleaf templates
+        // are HTML-based files located under "src/test/resources". Beside
+        // of the main HTML file, we also have partials like a footer or
+        // a header. We can re-use those partials in different documents.
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(HTML);
+        templateResolver.setCharacterEncoding(UTF_8);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        // The data in our Thymeleaf templates is not hard-coded. Instead,
+        // we use placeholders in our templates. We fill these placeholders
+        // with actual data by passing in an object. In this example, we will
+        // write a letter to "John Doe".
+        //
+        // Note that we could also read this data from a JSON file, a database
+        // a web service or whatever.
+        Data data = exampleDataForJohnDoe();
+
+        Context context = new Context();
+        context.setVariable("data", data);
+
+        // Flying Saucer needs XHTML - not just normal HTML. To make our life
+        // easy, we use JTidy to convert the rendered Thymeleaf template to
+        // XHTML. Note that this might not work for very complicated HTML. But
+        // it's good enough for a simple letter.
+        String renderedHtmlContent = templateEngine.process("template", context);
+        String xHtml = convertToXhtml(renderedHtmlContent);
+
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.getFontResolver().addFont("Code39.ttf", IDENTITY_H, EMBEDDED);
+
+        // FlyingSaucer has a working directory. If you run this test, the working
+        // directory
+        // will be the root folder of your project. However, all files (HTML, CSS, etc.)
+        // are
+        // located under "/src/test/resources". So we want to use this folder as the
+        // working
+        // directory.
+        String baseUrl = FileSystems.getDefault().getPath("src", "test", "resources").toUri().toURL().toString();
+        renderer.setDocumentFromString(xHtml, baseUrl);
+        renderer.layout();
+
+        // And finally, we create the PDF:
+        OutputStream outputStream = new FileOutputStream(OUTPUT_FILE);
+        renderer.createPDF(outputStream);
+        outputStream.close();
+    }
+
+    private Data exampleDataForJohnDoe() {
+        Data data = new Data();
+        data.setFirstname("John");
+        data.setLastname("Doe");
+        data.setStreet("Example Street 1");
+        data.setZipCode("12345");
+        data.setCity("Example City");
+        return data;
+    }
+
+    static class Data {
+        private String firstname;
+        private String lastname;
+        private String street;
+        private String zipCode;
+        private String city;
+
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
+
+        public String getLastname() {
+            return lastname;
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getZipCode() {
+            return zipCode;
+        }
+
+        public void setZipCode(String zipCode) {
+            this.zipCode = zipCode;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+    }
+
+    private String convertToXhtml(String html) throws UnsupportedEncodingException {
+        Tidy tidy = new Tidy();
+        tidy.setInputEncoding(UTF_8);
+        tidy.setOutputEncoding(UTF_8);
+        tidy.setXHTML(true);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(html.getBytes(UTF_8));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        tidy.parseDOM(inputStream, outputStream);
+        return outputStream.toString(UTF_8);
+    }
+
+}
---
+++ src/test/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        FlyingSaucerTest test = new FlyingSaucerTest();
+        try {
+            test.generatePdf();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
